cd9f22bc55fe9a31544c5fea7e1ae217274078ee
qinhong.wang <KJ69BG@insim.biz>
2019-06-18 14:16:04 +0900

    update

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  ActivatedRoute
+} from '@angular/router';
+import {
+  products
+} from '../products';
 
 @Component({
   selector: 'app-product-details',
@@ -7,9 +16,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+  public product;
+
+  constructor(
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.product = products[params.get('productId')];
+    });
   }
 
 }
