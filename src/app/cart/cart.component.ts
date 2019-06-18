f95d5edf50d9fcfe7df1fd18d011bc03ec892313
qinhong.wang <KJ69BG@insim.biz>
2019-06-18 14:43:21 +0900

    1

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  CartService
+} from '../cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -7,9 +13,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+  public items: any[];
+  public totalPrice: number;
+
+  constructor(
+    private cartService: CartService
+  ) {
+    this.items = this.cartService.getItems();
+  }
 
   ngOnInit() {
+    let sum = 0;
+    this.items.forEach(ele => {
+      sum += Number(ele.price);
+    });
+    this.totalPrice = sum;
   }
 
 }
